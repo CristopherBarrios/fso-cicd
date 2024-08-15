33a2b0b7c9350b02fdc020e501e0cd26beb9766c
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-08-14 20:48:31 -0600

    11.20 v9

--- phonebook-backend/webpack.config.js
+++ phonebook-backend/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   entry: './index.js',
@@ -6,6 +6,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  mode: 'production', 
+  target: 'node',
   module: {
     rules: [
       {
@@ -19,5 +21,28 @@ module.exports = {
         }
       }
     ]
+  },
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "path": require.resolve("path-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "assert": require.resolve("assert/"),
+      "url": require.resolve("url/"),
+      "zlib": require.resolve("browserify-zlib"),
+      "vm": require.resolve("vm-browserify"),
+      "async_hooks": require.resolve("async_hooks-browserify"),
+      "querystring": require.resolve("querystring-es3")
+    }
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    http: 'empty'
   }
-}
+};
