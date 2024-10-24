02b04c16a3114dbcc639f8f51f8630319d336559
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-10-23 19:58:17 -0600

    11.20 v33

--- phonebook-frontend/app.js
+++ phonebook-frontend/app.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -14,9 +14,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
